2667cde9eca8b3e42dea22cd05369faa76de79f6
Konrad Lemanski <uriootv@protonmail.com>
2022-03-30 19:27:36 +0200

    database.js usuniety

--- database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-
-// create the connection to database
-module.exports = mysql.createConnection({
-  host: '3.120.172.22',
-  user: 'gdansk_uczen',
-  password: '123456',
-  database: 'projekt_gdynia'
-});
