117d8720a220897d8d8942c1ca338e79c3ad5a3b
Bartek <bartekoffice1@gmail.com>
2022-03-30 18:59:04 +0200

    pliki konfiguracyjne

---
+++ database.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+module.export = mysql.createConnection({
+  host: '3.120.172.22',
+  user: 'gdansk_uczen',
+  password: '123456',
+  database: 'projekt_gdynia'
+});
