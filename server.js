f22e8cf6a72702be0ca886fe448b41f741df961e
Bartek <bartekoffice1@gmail.com>
2022-03-30 19:04:28 +0200

    dodanie modulow npm

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const db = require('./database')
+
+
+
+
+
 
 app.get('/',  (req, res) => {
   res.send('Hello World')
 
-  
+
 })
 
 app.listen(3000)
