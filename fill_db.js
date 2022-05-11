c8fc8af775a4c508be793bb194a4fddf82208f3a
Iru21 <33987046+Iru21@users.noreply.github.com>
2022-05-11 19:07:31 +0200

    tidy up script, set course list to ids instead of names

--- fill_db.js
+++ fill_db.js
@@ -1,44 +1,25 @@
 require("dotenv").config()
-const { faker } = require('@faker-js/faker')
+const { faker } = require("@faker-js/faker")
 const db = require("./database")
-
-//for(var x =0; x<10;x++){
 
 const randomName = faker.name.firstName()
 const randomSurname = "Kowalski"
 const age = Math.floor(Math.random() * 100)
-const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+const randomEmail = faker.internet.email() // Kassandra.Haley@erich.biz
 const password = faker.internet.password()
 const nick = faker.animal.type()
 
-const courses = ["Programowanie w C++","Programowanie w C","Programowanie w Java","Programowanie w Python"]
+const courses = [1, 2, 3, 4]
 const randomCourses = Math.floor(Math.random() * courses.length)
-let coursesPick = (randomCourses, courses[randomCourses])
-const admin = 0
-var id 
 
-
-
-  db.query(
-    'SELECT * FROM `users` ',
-    
-    function(err, results) {
-      console.log(results.length);
-      id = results.length++
-    }
-    
-  );
-
-
-
-//db.query(`INSERT INTO users (firstname, lastname, age, email, password, nick, courses, admin) VALUES(${randomName}, ${randomSurname}, ${age}, ${randomEmail}, ${password}, ${nick}, ${coursesPick}, ?)`)
-
-db.promise()
-        .query(`INSERT INTO users (id,firstname, lastname,username, age, email, courses, password, admin) VALUES(?,?,?,?,?,?,?,?,?)`, [
-            id,randomName, randomSurname,nick, age, randomEmail,coursesPick, password, admin
-        ])
-
-//db.promise().query(`SELECT * FROM users WHERE firstname="user"`)
-
-    console.log(randomName,age, randomEmail, password, nick,coursesPick, admin)
-
+db.promise().query(`INSERT INTO users (firstname, lastname,username, age, email, courses, password, admin) VALUES(?,?,?,?,?,?,?,?)`, [
+    randomName,
+    randomSurname,
+    nick,
+    age,
+    randomEmail,
+    courses[randomCourses],
+    password,
+    0,
+])
+console.log(randomName, age, randomEmail, password, nick, coursesPick, admin)
