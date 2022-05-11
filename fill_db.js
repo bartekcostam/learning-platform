bec416cf95080a45350b732091206d7be5e4e677
Iru21 <33987046+Iru21@users.noreply.github.com>
2022-05-11 19:17:29 +0200

    fix variable

--- fill_db.js
+++ fill_db.js
@@ -7,19 +7,23 @@ const randomSurname = "Kowalski"
 const age = Math.floor(Math.random() * 100)
 const randomEmail = faker.internet.email() // Kassandra.Haley@erich.biz
 const password = faker.internet.password()
-const nick = faker.animal.type()
+const username = faker.animal.type()
 
 const courses = [1, 2, 3, 4]
 const randomCourses = Math.floor(Math.random() * courses.length)
 
-db.promise().query(`INSERT INTO users (firstname, lastname,username, age, email, courses, password, admin) VALUES(?,?,?,?,?,?,?,?)`, [
-    randomName,
-    randomSurname,
-    nick,
-    age,
-    randomEmail,
-    courses[randomCourses],
-    password,
-    0,
-])
-console.log(randomName, age, randomEmail, password, nick, coursesPick, admin)
+;async () => {
+    db.promise().query(`INSERT INTO users (firstname, lastname,username, age, email, courses, password, admin) VALUES(?,?,?,?,?,?,?,?)`, [
+        randomName,
+        randomSurname,
+        username,
+        age,
+        randomEmail,
+        courses[randomCourses],
+        password,
+        0,
+    ])
+
+    const newUser = (await db.promise().query(`SELECT * FROM users WHERE email = ?`, [email]))[0]
+    console.log(newUser)
+}
